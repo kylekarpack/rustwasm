0ddeb6a65b83224d58be0694113f8ac8f6ce2c08
Kyle Karpack <kylekarpack@gmail.com>
2021-06-27 17:56:04 -0700

    Try to implement more

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,37 @@
 extern crate cfg_if;
+extern crate reqwest;
+extern crate select;
 extern crate wasm_bindgen;
 
 mod utils;
 
 use cfg_if::cfg_if;
+use select::document::Document;
+use select::predicate::Name;
 use wasm_bindgen::prelude::*;
 
 cfg_if! {
-    // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-    // allocator.
-    if #[cfg(feature = "wee_alloc")] {
-        extern crate wee_alloc;
-        #[global_allocator]
-        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-    }
+		// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+		// allocator.
+		if #[cfg(feature = "wee_alloc")] {
+				extern crate wee_alloc;
+				#[global_allocator]
+				static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+		}
 }
 
 #[wasm_bindgen]
-pub fn greet() -> String {
-    "Hello, wasm-worker!".to_string()
+pub async fn greet() -> String {
+	let url = "https://www.lexico.com/en/definition/sound";
+
+	let mut resp = reqwest::get(url).await.text().await;
+	return resp;
+	// assert!(resp.status().is_success());
+
+	// Document::from_read(resp)
+	// 	.unwrap()
+	// 	.find(Name("a"))
+	// 	.filter_map(|n| n.attr("href"))
+	// 	.for_each(|x| println!("{}", x));
+	// "Hello, wasm-worker modified!".to_string()
 }
